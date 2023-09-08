a8100973eff0a49463fb48ce72a95b2164db2f88
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-09-08 19:19:10 +0300

    0.2 FIREBASE DB 🔥 test

--- app/src/main/java/com/example/readerapp/MainActivity.kt
+++ app/src/main/java/com/example/readerapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.readerapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,18 +12,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.readerapp.ui.theme.ReaderAppTheme
+import com.google.api.LogDescriptor
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ReaderAppTheme {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    val db = FirebaseFirestore.getInstance()
+                    val user: MutableMap<String, Any> = HashMap()
+                    user["firstName" ] = "John"
+                    user["lastName"] = "Doe"
+
+                    db.collection("users").add(user)
+                        .addOnSuccessListener { Log.d("FDB", "onCreate: ${it.id}") }
+                        .addOnFailureListener{Log.d("FDB", "onCreate: $it")}
                 }
             }
         }
